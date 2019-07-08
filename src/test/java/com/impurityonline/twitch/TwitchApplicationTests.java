d9961baa71b17081c49ec0c821b80bfaec009dcb
Tyler Kokoszka <techkoko77@gmail.com>
2019-07-08 08:36:53 -0400

    Added tests

--- src/test/java/com/impurityonline/twitch/TwitchApplicationTests.java
+++ src/test/java/com/impurityonline/twitch/TwitchApplicationTests.java
@@ -1,16 +1,32 @@
 package com.impurityonline.twitch;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TwitchApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@org.junit.jupiter.api.Test
+	@DisplayName("The application entry works")
+	public void application_entry() {
+		TwitchApplication.main(new String[]{"Apples", "Bananas"});
+		assertNotNull(applicationContext);
 	}
 
+	@Test
+	@DisplayName("The application context loads")
+	public void context_loads() {
+		assertNotNull(applicationContext);
+	}
 }
---
+++ src/test/java/com/impurityonline/twitch/utils/AbstractTest.java
@@ -0,0 +1,44 @@
+package com.impurityonline.twitch.utils;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * @author tmk2003
+ */
+public abstract class AbstractTest {
+
+    private final ObjectMapper _objectMapper = new ObjectMapper();
+
+    /**
+     * Map the object to json string
+     *
+     * @param obj an object to be converted
+     * @return a json string of the object
+     * @throws JsonProcessingException if it cannot be processed
+     */
+    protected String mapToJson(Object obj)
+            throws JsonProcessingException {
+        return _objectMapper.writeValueAsString(obj);
+    }
+
+    /**
+     * Convert the json string to an object
+     *
+     * @param json is the json string
+     * @param clazz is the object that this string converts to
+     * @param <T> the type of the object to convert to
+     * @return The object we have converted from json
+     * @throws JsonParseException if it cannot be parsed
+     * @throws JsonMappingException if it cannot be mapped
+     * @throws IOException if there is an IO exception
+     */
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+        return _objectMapper.readValue(json, clazz);
+    }
+}
