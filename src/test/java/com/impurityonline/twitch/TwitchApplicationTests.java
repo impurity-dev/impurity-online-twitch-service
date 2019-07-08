771009febf10976f454bbd6c051d658705154fd3
Tyler Kokoszka <techkoko77@gmail.com>
2019-07-08 09:53:09 -0400

    updated gradle build

--- src/test/java/com/impurityonline/twitch/TwitchApplicationTests.java
+++ src/test/java/com/impurityonline/twitch/TwitchApplicationTests.java
@@ -2,22 +2,19 @@ package com.impurityonline.twitch;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TwitchApplicationTests {
 
 	@Autowired
 	private ApplicationContext applicationContext;
 
-	@org.junit.jupiter.api.Test
+	@Test
 	@DisplayName("The application entry works")
 	public void application_entry() {
 		TwitchApplication.main(new String[]{"Apples", "Bananas"});
